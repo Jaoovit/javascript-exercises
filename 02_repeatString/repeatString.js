17dc430fdc32f350e6537e931de01f5467b6fe12
João Oliveira <jaoovit@gmail.com>
2023-10-23 18:06:05 +0100

    create a repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
-};
-
+function repeatStringNumTimes(string, times) {
+    var repeatedString = "";
+    while (times > 0) {
+      repeatedString += string;
+      times--;
+    }
+    return repeatedString;
+  }
+  repeatStringNumTimes("hey", 3);
 // Do not edit below this line
-module.exports = repeatString;
+module.exports = repeatStringNumTimes;
