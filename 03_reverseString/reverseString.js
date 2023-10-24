857e48b5e25ab3c647401989d44dfee8aa3a8928
João Oliveira <jaoovit@gmail.com>
2023-10-24 10:28:31 +0100

    create a reverseString function returning .split, .reverse and .join

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+function reverseString(str) {
+    
+    /*let splitString = str.split("");
+    console.log(splitString)
+    let reverseArray = splitString.reverse();
+    console.log(reverseArray)
+    let joinArray = reverseArray.join("");
+    console.log(joinArray)*/
+    
+
+    return str.split("").reverse().join("");
 
 };
-
+reverseString("Hello there")
 // Do not edit below this line
 module.exports = reverseString;
