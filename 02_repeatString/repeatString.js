3ef86e21964c37b96e787dedb142ea774eb73a9b
João Oliveira <jaoovit@gmail.com>
2023-10-31 08:54:16 +0000

    review the repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,13 +1,11 @@
 function repeatStringTimes(string, times) {
-    if (times < 0)
-        return "ERROR"
-    else if (times >= 0)
-        return string.repeat(times)
-    else 
-    return "";
+    if (times < 0) {return 'ERROR'}
+        else {return string.repeat(times)}
+    
 }
+console.log(repeatStringTimes('hey', 3))
 
-repeatStringTimes('hey', 3)
+
 
 // Do not edit below this line
 module.exports = repeatStringTimes;
