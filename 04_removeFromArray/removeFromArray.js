a4ffd8eeb5b9f30d2c03c3287f2d7eb1fcd420ac
João Oliveira <jaoovit@gmail.com>
2023-11-24 10:05:27 +0000

    passed in the first test 'removeFromArray'

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,11 +2,17 @@ function removeFromArray() {
     
     const arr = [1, 2, 3, 4];
 
-    return arr
+    const halfbeforearr = arr.slice(0,2);
+
+    const halfafterarr = arr.slice(3);
+
+    const copyWithoutThree = halfbeforearr.concat(halfafterarr);
+
+    return copyWithoutThree
  
 }
 
-removeFromArray ()
+console.log(removeFromArray())
 
 // Do not edit below this line
 module.exports = removeFromArray;
