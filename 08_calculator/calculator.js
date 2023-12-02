372c31f57739b4034e8b55a07051d66b36da537f
João Oliveira <jaoovit@gmail.com>
2023-12-02 11:47:26 +0000

    exercise 08: solve the power issue

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -15,8 +15,8 @@ const multiply = function(array) {
   return array.reduce((total, current) => total * current, 1);
 };
 
-const power = function() {
-	
+const power = function(num, times) {
+  return Math.pow(num, times)
 };
 
 const factorial = function() {
