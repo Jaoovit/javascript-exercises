5be7a7024e859de79eed3219edf1d947c685e831
João Oliveira <jaoovit@gmail.com>
2023-12-02 11:57:59 +0000

    exercise 08: solve the factorial issue

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -19,8 +19,14 @@ const power = function(num, times) {
   return Math.pow(num, times)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num < 0)
+    return -1;
+  else if (num == 0)
+    return 1;
+  else {
+    return (num * factorial(num - 1));
+  }
 };
 
 // Do not edit below this line
