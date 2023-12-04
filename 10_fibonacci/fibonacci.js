4c9cf08a19f0d60bacbd29639f26354ecee83945
João Oliveira <jaoovit@gmail.com>
2023-12-04 08:01:28 +0000

    exercise 11: solve the problem using array.map

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
+const fibonacci = function(num, memo) {
+    memo = memo || {};
 
+  if (memo[num]) return memo[num];
+  if (num <= 1) return 1;
+
+  return memo[num] = fibonacci(num - 1, memo) + fibonacci(num - 2, memo);
 };
 
 // Do not edit below this line
